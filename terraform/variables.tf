5e2456a410e0dd623e2e7fb836fb9a3d84cfc92a
tamimyx <tamim.schapakow@proton.me>
2026-05-19 13:42:23 +0200

    chore: move tfvars to terraform directory and clean up workflow path

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,3 @@
 variable "resource_group_name" {}
 
 variable "location" {}
-
-variable "github_org" {
-  type = string
-}
-
-variable "github_repo" {
-  type = string
-}
-
-variable "production_environment" {
-  type    = string
-  default = "production"
-}
-
-variable "main_branch" {
-  type    = string
-  default = "main"
-}
-
-variable "subscription_id" {
-  type = string
-}
-
-variable "tfstate_storage_account_id" {
-  type = string
-}
